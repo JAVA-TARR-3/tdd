cc05522d4b1e982d90fbbfb34fd53f6fbe0d6f23
Marcin Szupke <marcin.szupke@gmail.com>
2020-06-07 09:59:58 +0200

    testy sparametryzowane

--- src/test/java/ParameterizedTestDemoTest.java
+++ src/test/java/ParameterizedTestDemoTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,6 +23,38 @@ public class ParameterizedTestDemoTest {
         assertTrue(isPalindrome(word));
     }
 
+    @ParameterizedTest
+    @NullSource
+    @EmptySource
+    @ValueSource(strings = { "" })
+    public void nullOrEmpty(String text) {
+        assertTrue(isEmpty(text));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void nullAndEmpty(String text) {
+        assertTrue(isEmpty(text));
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = TimeUnit.class)
+    public void testWithEnumSource(TimeUnit timeUnit) {
+        assertNotNull(timeUnit);
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = TimeUnit.class, mode = EnumSource.Mode.EXCLUDE, names = { "SECONDS", "HOURS"})
+    public void testWithEnumSourceExclude(TimeUnit timeUnit) {
+        assertNotNull(timeUnit);
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringProvider")
+    public void testWithLocalMethod(String word) {
+        assertTrue(isPalindrome(word));
+    }
+
     private boolean isPalindrome(String word) {
 
         String reverse = "";
@@ -28,5 +63,17 @@ public class ParameterizedTestDemoTest {
         }
 
         return reverse.equals(word);
+    }
+
+    private boolean isEmpty(String word) {
+        if (word == null || word.length() == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    static Stream<String> stringProvider() {
+        return Stream.of("kajak", "rotor", "racecar");
     }
 }
